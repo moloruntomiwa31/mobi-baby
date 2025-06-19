78c28fa32eee2b5d86e374f73e58aa3f7d86ccdc
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2025-06-19 01:15:07 +0100

    settings page complete

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,40 +1,66 @@
-<!-- components/SettingsContent.vue -->
 <template>
     <SettingsLayout>
-    <div class="flex flex-col items-center justify-center mt-12 gap-8 mdlg:gap-6 h-full">
-      <div class="relative bg-gray rounded-xl w-full max-w-md flex flex-col items-center pt-16 pb-6">
-        <Avatar
-          size="4xl"
-          class="absolute left-1/2 -translate-x-1/2 -top-12 shadow-lg"
-        />
-        <BaseText content="Aminat" size="lg" bold class="mt-4" />
-      </div>
-  
-      <div class="bg-gray rounded-xl w-full max-w-md p-4">
-        <div class="flex items-center justify-between">
-          <BaseText content="Date Of Birth" bold />
-          <BaseText content="January 1, 2000" size="md" class="text-gray-600" />
-          <Pencil class="w-4 h-4" />
+      <div class="flex flex-col items-center justify-center mt-12 mdlg:mt-6 gap-6 mdlg:gap-4 h-full">
+        <div class="relative bg-gray rounded-xl w-full max-w-sm mdlg:max-w-md flex flex-col items-center pt-16 pb-6">
+          <Avatar
+            :size="isDesktop ? '2xl' : '4xl'"
+            class="absolute left-1/2 -translate-x-1/2 shadow-lg"
+            :class="[isDesktop ? '-top-6' : '-top-12']"
+          />
+          <BaseText content="Aminat Sherifat" size="lg" bold class="mt-4" />
         </div>
-      </div>
   
-      <div class="bg-gray rounded-xl w-full max-w-md p-4">
-        <div class="flex items-center justify-between">
-          <BaseText content="Language" bold />
-          <div class="flex items-center gap-2">
-            <BaseText content="English" size="md" class="text-gray-600" />
-            <ChevronRight class="w-5 h-5" />
+        <div class="bg-gray rounded-xl w-full max-w-sm mdlg:max-w-md p-4">
+          <div class="flex items-center justify-between">
+            <BaseText content="Date Of Birth" size="sm" bold />
+            <BaseText content="January 1, 2000" class="text-gray-600" />
+            <button @click="pickDate = !pickDate" class="cursor-pointer">
+              <Pencil class="w-4 h-4" />
+            </button>
           </div>
         </div>
-      </div>
   
-      <BaseText class="text-primaryAqua" size="sm">Need help or support?</BaseText>
-      <Button variant="yellow" size="rounded" class="w-full max-w-md">Logout</Button>
-    </div>
+        <div v-if="pickDate" class="bg-gray rounded-xl w-full max-w-sm mdlg:max-w-md p-4">
+          <DatePicker class="w-full" />
+        </div>
+  
+        <div class="bg-gray rounded-xl w-full max-w-sm mdlg:max-w-md p-4">
+          <div class="flex items-center justify-between">
+            <BaseText content="Language" size="sm" bold />
+            <div class="flex items-center gap-2">
+              <BaseText content="English" class="text-gray-600" />
+              <ChevronRight class="w-5 h-5" />
+            </div>
+          </div>
+        </div>
+  
+        <div class="bg-gray rounded-xl w-full max-w-sm mdlg:max-w-md p-4">
+          <BaseSelect
+            :placeholder="languages[0]"
+            :options="languages"
+            class="w-full"
+          />
+        </div>
+  
+        <BaseText class="text-primaryAqua" size="sm">Need help or support?</BaseText>
+  
+        <Button variant="yellow" size="rounded" class="w-full max-w-sm mdlg:max-w-md">
+          Logout
+        </Button>
+      </div>
     </SettingsLayout>
   </template>
   
   <script setup lang="ts">
   import { Pencil, ChevronRight } from "lucide-vue-next";
+  
+  const { isDesktop } = useScreen();
   useHead({ title: 'Settings' });
+  
+  const pickDate = ref(false);
+  
+  const languages = [
+    "English", "Yoruba", "Igbo", "Hausa"
+  ];
   </script>
+  
